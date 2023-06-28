4f51e53be5e99f455d5ae238cab6f684aeaf698b
Cleyson Lima <cleysonph@gmail.com>
2023-06-28 19:02:30 -0300

    02:03 - Criando os modelos da aplicação

---
+++ jobs/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class JobType(models.TextChoices):
+    FULL_TIME = "FULL_TIME", "Full Time"
+    PART_TIME = "PART_TIME", "Part Time"
+    FREELANCE = "FREELANCE", "Freelance"
+    INTERSHIP = "INTERSHIP", "Intership"
+
+
+class JobLevel(models.TextChoices):
+    JUNIOR = "JUNIOR", "Junior"
+    MIDDLE = "MIDDLE", "Middle"
+    SENIOR = "SENIOR", "Senior"
+
+
+class Job(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    company = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    job_type = models.CharField(max_length=20, choices=JobType.choices)
+    job_level = models.CharField(max_length=20, choices=JobLevel.choices)
+    salary = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    is_active = models.BooleanField(default=True)
+    skills = models.ManyToManyField("skills.Skill", related_name="jobs")
+
+    def __str__(self):
+        return self.title
--- setup/settings.py
+++ setup/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -40,6 +40,15 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
+
+THIRD_PARTY_APPS = []
+
+LOCAL_APPS = [
+    "jobs.apps.JobsConfig",
+    "skills.apps.SkillsConfig",
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
