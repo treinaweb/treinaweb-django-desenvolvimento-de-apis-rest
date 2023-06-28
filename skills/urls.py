b8d4bd7ccf8c3eacb817ceede1ff7520c868760b
Cleyson Lima <cleysonph@gmail.com>
2023-06-28 20:48:42 -0300

    04:05 - Implementando a rota de cadastro de skill

--- skills/urls.py
+++ skills/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
 from .views import SkillList
 
 
 app_name = "skills"
 urlpatterns = [
-    path("", SkillList.as_view(), name="list")
+    path("", csrf_exempt(SkillList.as_view()), name="list"),
 ]
--- skills/views.py
+++ skills/views.py
@@ -1,5 +1,6 @@
+import json
 from django.views import View
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpRequest
 
 from .models import Skill
 
@@ -9,3 +10,8 @@ class SkillList(View):
         skills = Skill.objects.all()
         data = [skill.to_json() for skill in skills]
         return JsonResponse(data, safe=False)
+
+    def post(self, request: HttpRequest):
+        body = json.loads(request.body)
+        skill = Skill.objects.create(name=body["name"])
+        return JsonResponse(skill.to_json())
