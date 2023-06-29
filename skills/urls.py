ecaf6a3d466f1c559dceed6604d292c944f76772
Cleyson Lima <cleysonph@gmail.com>
2023-06-28 22:01:51 -0300

    07:09 - Criando a rota de cadastro de Skill

--- skills/urls.py
+++ skills/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from django.views.decorators.csrf import csrf_exempt
 
 from .views import SkillList
 
 
 app_name = "skills"
 urlpatterns = [
-    path("", csrf_exempt(SkillList.as_view()), name="list"),
+    path("", SkillList.as_view(), name="list"),
 ]
--- skills/views.py
+++ skills/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Skill
 from .serializers import SkillSerializer
@@ -10,3 +11,9 @@ class SkillList(APIView):
         skills = Skill.objects.all()
         serializer = SkillSerializer(skills, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = SkillSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
