c85dce7e8db7f46f59ceb5c46e4701f1a9402b60
Cleyson Lima <cleysonph@gmail.com>
2023-06-29 19:23:24 -0300

    08:14 - Criando a rota de detalhes, edição e exclusão de Job

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import JobList
+from .views import JobList, JobDetail
 
 
 app_name = "jobs"
 urlpatterns = [
     path("", JobList.as_view(), name="list"),
+    path("<int:pk>", JobDetail.as_view(), name="detail"),
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 from .models import Job
 from .serializers import JobSerializer
@@ -8,7 +9,7 @@ from .serializers import JobSerializer
 
 class JobList(APIView):
     def get(self, request):
-        jobs = Job.objects.all()
+        jobs = Job.objects.filter(is_active=True)
         serializer = JobSerializer(jobs, many=True)
         return Response(serializer.data)
 
@@ -17,3 +18,26 @@ class JobList(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class JobDetail(APIView):
+    def __get_object(self, pk):
+        return get_object_or_404(Job, pk=pk, is_active=True)
+
+    def get(self, request, pk):
+        job = self.__get_object(pk)
+        serializer = JobSerializer(job)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        job = self.__get_object(pk)
+        serializer = JobSerializer(job, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def delete(self, request, pk):
+        job = self.__get_object(pk)
+        job.is_active = False
+        job.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
