5601493424ea8486ee99991ad9c900c539517f8b
Cleyson Lima <cleysonph@gmail.com>
2023-06-29 20:17:53 -0300

    09:15 - Adicionando validações customizadas

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -7,3 +7,10 @@ class JobSerializer(serializers.ModelSerializer):
     class Meta:
         model = Job
         exclude = ("is_active",)
+        extra_kwargs = {
+            "title": {"min_length": 10},
+            "salary": {"min_value": 1000},
+            "description": {"min_length": 10, "max_length": 500},
+            "company": {"min_length": 3},
+            "location": {"min_length": 10},
+        }
